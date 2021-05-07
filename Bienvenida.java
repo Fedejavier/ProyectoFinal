b1354f2a18ff5c4f62cecf5371f75e6d387c5f4d
Fedejavier <82660525+Fedejavier@users.noreply.github.com>
2021-05-07 11:31:33 -0300

    07/05/21-11:31

---
+++ Bienvenida.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+import java.awt.event.*;
+import java.awt.*;
+
+public class Bienvenida extends JFrame Implements ActionListener{
+
+    private JTextField textfields;
+    private JLabel label1, label2, label3, label4;
+    private JButton boton1;
+    
+}
